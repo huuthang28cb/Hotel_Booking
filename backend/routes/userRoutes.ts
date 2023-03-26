9e05bb39b0ce68b84df17b15fa0f1673f259350c
Huu Thang <huuthang28.work@gmail.com>
2023-03-26 17:19:50 +0700

    update code user

--- backend/routes/userRoutes.ts
+++ backend/routes/userRoutes.ts
@@ -1,9 +1,25 @@
 import express from 'express';
-import { register, login} from '../controllers/userController';
+import {
+    register,
+    login,
+    updateUser,
+    getSingleUser,
+    deleteUser,
+    getAll,
+    updateProfile,
+    updatePassword
+} from '../controllers/userController';
+import { protect, admin } from '../middlewares/authMiddleware';
 
 const router = express.Router();
 
+router.route("/").get(protect, admin, getAll);
 router.route("/register").post(register)
 router.route("/login").post(login);
+router.route("/:id").put(protect, admin, updateUser)
+    .get(protect, admin, getSingleUser)
+    .delete(protect, admin, deleteUser);
+router.route("/update").put(protect, updateProfile);
+router.route("/update/password").put(protect, updatePassword);
 
 export default router;
